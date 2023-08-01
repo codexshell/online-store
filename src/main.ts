7d5edbc01b94c9d12a53b1d12c7a6c925db17b73
codexshell <ericoderito@gmail.com>
2023-08-01 13:42:14 +0300

    feat: add the welcome page

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+
+import { AppModule } from './app.module';
+import * as hbs from 'hbs';
+import * as hbsUtils from 'hbs-utils';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -9,6 +12,10 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+  hbs.registerPartials(join(__dirname, '..', 'views/layouts'));
+  if (hbsUtils.registerWatchedPartials) {
+    hbsUtils.registerWatchedPartials(join(__dirname, '..', 'views/layouts'));
+  }
 
   await app.listen(3000);
 }
