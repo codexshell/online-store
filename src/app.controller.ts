7fe6d93016e9e612e42a0282a7d57cf9cc1611b1
codexshell <ericoderito@gmail.com>
2023-08-01 14:36:19 +0300

    chore: add a route and route handler for about

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,22 @@ export class AppController {
   @Get('/')
   @Render('index')
   index() {
-    // Add logic here if needed
+    return {
+      title: 'Home Page - Online Store',
+    };
+  }
+
+  @Get('/about')
+  @Render('about')
+  about() {
+    const viewData = [];
+    viewData['description'] = 'This is an about page...';
+    viewData['author'] = 'Developed by: Eric';
+    const title = 'About us - Online Store';
+    return {
+      title,
+      subtitle: 'About Us',
+      viewData,
+    };
   }
 }
