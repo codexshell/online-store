f76fb40aa2c11385c51ba4e0858ff0144d727756
codexshell <ericoderito@gmail.com>
2023-08-03 13:01:39 +0300

    chore: connect the app to the mysql database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { ProductsController } from './products.controller';
+import { DatabaseModule } from './database.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [AppController, ProductsController],
   providers: [],
 })
