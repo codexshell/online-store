78345cd1f88863231c63dc10ce077bc5f2e612ad
codexshell <ericoderito@gmail.com>
2023-08-14 13:08:43 +0300

    feat: add admin module and group related components

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ProductsController } from './products.controller';
 import { DatabaseModule } from './database.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AdminModule],
   controllers: [AppController, ProductsController],
   providers: [],
 })
