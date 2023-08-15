ca0e3ffd7aad655c15c88c0c6fffd4b2ac52e833
codexshell <ericoderito@gmail.com>
2023-08-15 16:40:24 +0300

    fix(register.hbs): add a `name` attribute to the password field
    Lack of the name attribute causes the request body to be sent without
    the password field

--- src/models/users.service.ts
+++ src/models/users.service.ts
@@ -11,7 +11,7 @@ export class UsersService {
     @InjectRepository(User) private usersRepository: Repository<User>,
   ) {}
 
-  async createOrUpdate(user: User) {
+  async createOrUpdate(user: User): Promise<User> {
     const hash = await bcrypt.hash(user.getPassword(), 10);
     user.setPassword(hash);
     return await this.usersRepository.save(user);
