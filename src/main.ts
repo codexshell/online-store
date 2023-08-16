d3fd222b3a7703208acb90094c5aa4b8a2a6a492
codexshell <ericoderito@gmail.com>
2023-08-16 07:56:26 +0300

    chore: configure session middleware
    The session function from express-session is used to configure sessions.
    The sessions are retreived from the req object and stored on the response
    object making them accessible to all views that are rendered during that
    req/res cycle

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-
-import { AppModule } from './app.module';
 import * as hbs from 'hbs';
 import * as hbsUtils from 'hbs-utils';
+import * as session from 'express-session';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
+
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
   hbs.registerPartials(join(__dirname, '..', 'views/layouts'));
   if (hbsUtils.registerWatchedPartials) {
     hbsUtils.registerWatchedPartials(join(__dirname, '..', 'views/layouts'));
   }
+
+  app.use(
+    session({
+      secret: 'nest-book',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+  app.use(function (req, res, next) {
+    res.locals.session = req.session;
+    next();
+  });
 
   await app.listen(3000);
 }
