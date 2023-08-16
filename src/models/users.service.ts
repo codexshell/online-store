de71dddded0d6fc7dd8628a3b5d5bd305f826e4d
codexshell <ericoderito@gmail.com>
2023-08-16 08:26:05 +0300

    chore: add a `login` method to the `userService`

--- src/models/users.service.ts
+++ src/models/users.service.ts
@@ -16,4 +16,23 @@ export class UsersService {
     user.setPassword(hash);
     return await this.usersRepository.save(user);
   }
+
+  async login(email: string, password: string) {
+    // check if user exists with the provided password
+    const user = await this.usersRepository.findOne({ where: { email } });
+    // if user does not exist, exit logging to the console, `user with email ${email} does not exist`
+    if (!user) {
+      console.log(`User with email ${email} does not exists`);
+      return;
+    }
+    // if user exists, extract password hash and match against provided password hash
+    const isMatch = bcrypt.compare(password, user.getPassword());
+    // if passwords do not match, exit logging to the console `invalid username/password`
+    if (!isMatch) {
+      console.log('Passwords do not match!');
+    }
+    // if passwords match log to the console `login successful`
+    console.log('Login successful');
+    return user;
+  }
 }
