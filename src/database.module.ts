f54329b266f9240f578eaf5828b88f2df0bcbfe1
codexshell <ericoderito@gmail.com>
2023-08-18 19:56:43 +0300

    chore: register the `Order` entity and `OrderService`
    The `Order` entity is registered to create a repository that allows
    interaction witht the order table
    The `OrderService` is registered in the providers declaration array of
    the app module, and exposed globally through the exports declaration
    array courtesy of the `Global` decorator

--- src/database.module.ts
+++ src/database.module.ts
@@ -5,6 +5,8 @@ import { ProductsService } from './models/products.service';
 import { Product } from './models/product.entity';
 import { User } from './models/user.entity';
 import { UsersService } from './models/users.service';
+import { Order } from './models/order.entity';
+import { OrdersService } from './models/orders.service';
 
 @Global()
 @Module({
@@ -19,9 +21,9 @@ import { UsersService } from './models/users.service';
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
-    TypeOrmModule.forFeature([Product, User]),
+    TypeOrmModule.forFeature([Product, User, Order]),
   ],
-  providers: [ProductsService, UsersService],
-  exports: [ProductsService, UsersService],
+  providers: [ProductsService, UsersService, OrdersService],
+  exports: [ProductsService, UsersService, OrdersService],
 })
 export class DatabaseModule {}
