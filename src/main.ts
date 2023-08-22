7b6498f6ba72ba08392a1017d12abbfa629ae925
codexshell <ericoderito@gmail.com>
2023-08-22 19:12:13 +0300

    build: allow for dynamic PORT configuration

--- src/main.ts
+++ src/main.ts
@@ -44,6 +44,10 @@ async function bootstrap() {
     }
   });
 
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+
+  await app.listen(PORT, () =>
+    console.log(`Application started on port ${PORT}`),
+  );
 }
 bootstrap();
